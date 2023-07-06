8cc3c839fddf25ccfc1d264c38a4bd945ca1d82c
alpha-jung <shter7119@gmail.com>
2023-07-06 17:59:27 +0900

    이벤트, 공지사항, 컨텐츠에 관련된 Type 정의

---
+++ src/types/News.d.ts
@@ -0,0 +1,15 @@
+type Notices = {
+    Date: string,
+    Link: string,
+    Title: string,
+    Type: string,
+}
+
+type Events = {
+    EndDate: string,
+    Link: string,
+    RewardDate: string,
+    StartDate: string,
+    Thumbnail: string,
+    Title: string,
+}
