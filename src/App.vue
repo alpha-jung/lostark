31bcc963852b899ef155632c83d624fcf6513b71
alpha-jung <shter7119@gmail.com>
2023-07-07 21:32:20 +0900

    LostArk API 페이지의 네비게이션션 부분 작성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">LostArk API</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <router-link to="/">Home</router-link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ranking</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search" @keydown.enter="searchCharacter">
+          <input class="form-control me-2" type="search" placeholder="캐릭터 검색" aria-label="Search" @input="setSearchText" :value="searchText">
+          <button class="btn btn-outline-success" type="submit" @click="searchCharacter">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <router-view></router-view>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { ref, defineComponent} from 'vue';
+import router from './router';
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    let searchText = ref('');
+
+    function setSearchText(e: Event) {
+      let text = (e.target as HTMLInputElement).value;
+      searchText.value = text;
+    }
+
+    function searchCharacter(e: any) {
+      e.preventDefault();
+      console.log(searchText.value);
+
+      router.push(`/character/${searchText.value}`);
+    }
+
+    return { searchText, setSearchText, searchCharacter };
   }
-}
+})
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
