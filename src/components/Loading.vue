2bbdd9f6c1d44e55a55f99f90f61e2ee5a4b99f8
alpha-jung <shter7119@gmail.com>
2023-07-07 21:33:41 +0900

    로딩 화면 컴포넌트

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+    name: 'Loading',
+})
+</script>
+
+<style>
+
+</style>
